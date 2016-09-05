8125d69a781c7df39f545814da4dee212dc014c5
Kate Hart <kejt.hart@gmail.com>
2016-09-04 18:25:41 -0700

    Added files for Travis CI

---
+++ karma.conf.js
@@ -0,0 +1,64 @@
+// Karma configuration
+'use strict'
+
+module.exports = function (config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha', 'sinon-chai'],
+
+    // list of files / patterns to load in the browser
+    files: [
+      'js/mock/*.js',
+      'js/**/*.js',
+      'tests/*.js'
+    ],
+
+    // list of files to exclude
+    exclude: [],
+
+    // optionally, configure the reporter
+    // text displays it within the console (alternative: text-summary)
+    // lcov creates a codecov compatible report
+    coverageReporter: {
+      reporters: [
+		// generates ./coverage/lcov.info
+		{type:'lcovonly', subdir: '.'},
+		// generates ./coverage/coverage-final.json
+		{type:'json', subdir: '.'},
+      ]
+    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    // coverage is from karma-coverage and provides Istanbul code coverage reports
+    reporters: ['mocha', 'coverage'],
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
